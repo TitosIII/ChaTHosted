9b41e0a6030ef56d479c67d3847074db687e9285
TitosIII <carloscw0304@gmail.com>
2022-11-12 17:32:07 -0600

    beta host version

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 import express from "express";
 import bodyParser from "body-parser";
 import http from "http";
-import { Socket } from "socket.io";
+import { Server } from "socket.io";
 import {PORT} from "./config/deploy.js";
 import {router} from "./config/router.js";
 
@@ -9,25 +9,70 @@ const app = express();
 app.use(express.static("public"))
 app.use(bodyParser.json())
 app.use("/",router);
-app.use(express.urlencoded({extended:false}))
+app.use(express.urlencoded({extended:true}))
 const server = http.Server(app);
-var io = Socket(server)
+const io = new Server(server);
+
+var users = [];
+var userWrt = [];
+
+function userConnected(Socket){
+    io.emit("userConnected", users);
+}
+
+
+server.listen(PORT, () =>{
+    console.log(`Escuchando en el puerto ${PORT}`);
+});
 
 io.on("connection", function(Socket){
     console.log(Socket.id+" conectado.");
-    io.emit("connect",{txt:`${Socket.id} conectado.<br>`});
+    userConnected(Socket);
 
-    io.on("disconnect", ()=>{
+    Socket.on("disconnect", ()=>{
         console.log(`${Socket.id} desconectado.`);
-        io.emit("disconnected", {txt: `${Socket.id} desconectado.`});
+        if (Socket.name != null){
+            users = users.filter(function(User){
+                return User != Socket.name;
+            });
+            userWrt = userWrt.filter(function(User){
+                return User != Socket.name;
+            });
+            var data = {name : Socket.name, userL : users, userWL: userWrt}
+            io.emit("userDisconnected", data);
+        }
     });
 
-    io.on("msn", (data)=>{
-        io.emit("msn", data);
+    Socket.on("newMessage", (msg)=>{
+        userWrt = userWrt.filter(function(User){
+            return User != Socket.name;
+        });
+        io.emit("userWritting", userWrt);
+        var data = {name : Socket.name, message : msg}
+        io.emit("newMessage", data);
     });
 
-    io.on("wrt", (user)=>{
-        io.broadcast.emit("wrt", user);
+    Socket.on("userConnected", (data)=>{
+        users.push(data);
+        Socket.name = data;
+        userConnected(Socket);
+    });
+
+    Socket.on("userWritting", ()=>{
+        var index = userWrt.indexOf(Socket.name);
+        if (index === -1){
+            userWrt.push(Socket.name);
+        }
+        io.emit("userWritting", userWrt);
+        console.log(userWrt);
+    });
+
+    Socket.on("userNotWritting",()=>{
+        userWrt = userWrt.filter(function(User){
+            return User != Socket.name;
+        });
+        io.emit("userWritting", userWrt);
+        console.log(userWrt);
     });
 });
 
